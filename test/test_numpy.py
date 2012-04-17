2d0f8ceb93ac7e98c0f6ed90cfcf8029e8ba582b
David Stuebe <dstuebe@asascience.com>
2012-04-16 22:36:00 -0700

    String test is working

--- test/test_numpy.py
+++ test/test_numpy.py
@@ -5,6 +5,7 @@ from nose import main
 from nose.tools import *
 import unittest
 
+import collections
 import time
 import numpy
 import random
@@ -31,39 +32,52 @@ def encode_numpy( obj):
         raise TypeError('Unknown type in user specified encoder')
     return obj
 
-
+count =0
 
 class NumpyMsgPackTestCase(unittest.TestCase):
 
     _decoder = None
     _encoder = None
 
-    types = {
-        'int8':('int8',random.randint,(-(1 << 7), (1 << 7)-1)),
-        'int16':('int16',random.randint,(-(1 << 15), (1 << 15)-1)),
-        'int32':('int32',random.randint,(-(1 << 31), (1 << 31)-1)),
-        'int64':('int64',random.randint,(-(1 << 63), (1 << 63)-1)),
+    types = collections.OrderedDict(
+        [
 
-        'uint8':('uint8',random.randint,(0, (1 << 8)-1)),
-        'uint16':('uint16',random.randint,(0, (1 << 16)-1)),
-        'uint32':('uint32',random.randint,(0, (1 << 32)-1)),
-        'uint64':('uint64',random.randint,(0, (1 << 64)-1)),
+        ('boolean',('bool',random.randint,(0, 1)) ),
+
+        ('|S1',('|S1', lambda o: chr(count) , (None,) ) ),
+        ('|S16',('|S16', lambda o: chr(count)*16 , (None,) ) ),
+
+        ('int8',('int8',random.randint,(-(1 << 7), (1 << 7)-1)) ),
+        ('int16',('int16',random.randint,(-(1 << 15), (1 << 15)-1)) ),
+        ('int32',('int32',random.randint,(-(1 << 31), (1 << 31)-1)) ),
+        ('int64',('int64',random.randint,(-(1 << 63), (1 << 63)-1)) ),
+
+        ('uint8',('uint8',random.randint,(0, (1 << 8)-1)) ),
+        ('uint16',('uint16',random.randint,(0, (1 << 16)-1)) ),
+        ('uint32',('uint32',random.randint,(0, (1 << 32)-1)) ),
+        ('uint64',('uint64',random.randint,(0, (1 << 64)-1)) ),
 
 
-        'float32_eps':('float32',lambda o: 1+o ,(numpy.finfo('float32').eps,)),
-        'float32_epsneg':('float32',lambda o: 1-o ,(numpy.finfo('float32').epsneg,)),
-        'float32':('float32',numpy.random.uniform,(numpy.finfo('float32').min, numpy.finfo('float32').max)),
+        ('float16_eps',('float16',lambda o: 1+o ,(numpy.finfo('float16').eps,)) ),
+        ('float16_epsneg',('float16',lambda o: 1-o ,(numpy.finfo('float16').epsneg,)) ),
+        ('float16',('float16',numpy.random.uniform,(numpy.finfo('float16').min, numpy.finfo('float16').max)) ),
 
-        'float64_eps':('float64',lambda o: 1+o ,(numpy.finfo('float64').eps,)),
-        'float64_epsneg':('float64',lambda o: 1-o ,(numpy.finfo('float64').epsneg,)),
-        'float64':('float64',numpy.random.uniform,(numpy.finfo('float64').min, numpy.finfo('float64').max)),
+        ('float32_eps',('float32',lambda o: 1+o ,(numpy.finfo('float32').eps,)) ),
+        ('float32_epsneg',('float32',lambda o: 1-o ,(numpy.finfo('float32').epsneg,)) ),
+        ('float32',('float32',numpy.random.uniform,(numpy.finfo('float32').min, numpy.finfo('float32').max)) ),
 
-        'complex64':('complex64',lambda a,b: numpy.complex(numpy.random.uniform(a,b), numpy.random.uniform(a,b)) ,(numpy.finfo('float64').min, numpy.finfo('float64').max)),
+        ('float64_eps',('float64',lambda o: 1+o ,(numpy.finfo('float64').eps,)) ),
+        ('float64_epsneg',('float64',lambda o: 1-o ,(numpy.finfo('float64').epsneg,)) ),
+        ('float64',('float64',numpy.random.uniform,(numpy.finfo('float64').min, numpy.finfo('float64').max)) ),
 
-    }
+        ('complex64',('complex64',lambda a,b: numpy.complex(numpy.random.uniform(a,b), numpy.random.uniform(a,b)) ,(numpy.finfo('float32').min, numpy.finfo('float32').max)) ),
+        ('complex128',('complex128',lambda a,b: numpy.complex(numpy.random.uniform(a,b), numpy.random.uniform(a,b)) ,(numpy.finfo('float64').min, numpy.finfo('float64').max)) ),
 
-    #shapes = ((3,4), (9,12,18), (10,10,10,10))
-    shapes = ((100,100,10,10),)
+        ]
+    )
+
+    shapes = ((3,4), (9,12,18), (10,10,10,10))
+    #shapes = ((100,100,10,10),)
 
 
     def __init__(self, *args, **kwargs):
@@ -74,8 +88,11 @@ class NumpyMsgPackTestCase(unittest.TestCase):
 
     def test_all(self):
 
-        for type,(type, func, args) in self.types.iteritems():
-            for shape in self.shapes:
+        for shape in self.shapes:
+            print "========================"
+            print "========================"
+            print "========================"
+            for type,(type, func, args) in self.types.iteritems():
                 print "Running type: %s, shape: %s" % (type, str(shape))
                 self.run_it(self._encoder, self._decoder, type, func, args, shape)
 
@@ -84,7 +101,9 @@ class NumpyMsgPackTestCase(unittest.TestCase):
 
         array = numpy.zeros(shape, type)
 
+        count = 0
         for x in numpy.nditer(array, op_flags=['readwrite']):
+            count +=1
             x[...] = func(*args)
 
         tic = time.time()
